c9f8177ebc0814123a8bb510bee4a8f210767153
phuhuykoxl <phuhuy02003@gmail.com>
2025-06-13 00:44:09 +0700

    update

--- seq2seq/src/train_alignment_distill.py
+++ seq2seq/src/train_alignment_distill.py
@@ -200,8 +200,8 @@ def main():
             "labels": lab_enc["input_ids"]
         }
 
-    train_ds = train_ds.map(preprocess, batched=True, cache_file_names=None,remove_columns=train_ds.column_names)
-    val_ds = val_ds.map(preprocess, batched=True, cache_file_names=None,remove_columns=val_ds.column_names)
+    train_ds = train_ds.map(preprocess, batched=True, load_from_cache_file=False,remove_columns=train_ds.column_names)
+    val_ds = val_ds.map(preprocess, batched=True, load_from_cache_file=False,remove_columns=val_ds.column_names)
     # Optional sanity-check (will raise immediately if something is wrong)
     sample = train_ds[0]
     for k in ("input_ids_fwd", "attention_mask_fwd",
